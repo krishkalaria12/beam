46466b291e603587db6da0607ca9e4332d48a055
krish <codewithkrish12@gmail.com>
2026-04-01 22:18:26 +0530

    refactor: move to tailwind from css

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   image: {
     domains: ['liqgcawbf9.ufs.sh'],
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
